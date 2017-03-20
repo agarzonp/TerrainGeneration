958242feb98e64b057fa0b7e16b59fac8279a87f
agarzonp <agarzonp@gmail.com>
2017-03-20 18:11:29 +0100

    Project initial setup

---
+++ TerrainGeneration/src/TerrainGeneration/TerrainGeneration.h
@@ -0,0 +1,227 @@
+#ifndef SPLINE_CAM_H
+#define SPLINE_CAM_H
+
+#include "../Input/Input.h"
+#include "../Shaders/Shader.h"
+
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/quaternion.hpp"
+#include "glm/gtx/quaternion.hpp"
+#include "Camera/FreeCamera.h"
+
+#include <cassert>
+#include <memory>
+
+class TerrainGeneration : public InputListener
+{
+
+public:
+	TerrainGeneration() 
+	{
+		Init();
+	}
+
+	~TerrainGeneration() 
+	{
+		Terminate();
+	}
+
+	void OnKeyPressed(int key) override 
+	{
+		switch(key)
+		{
+			case GLFW_KEY_F1:
+				ToggleWireframeMode();
+				break;
+			default:
+				break;
+		}
+	};
+
+	void OnKeyReleased(int key) override { };
+	void OnMouseButtonPressed(int button, double x, double y) override { };
+	void OnMouseButtonReleased(int button, double x, double y) override { };
+	void OnMouseScroll(double xoffset, double yoffset) override { };
+	
+	void OnMouseMove(double x, double y) 
+	{
+	}
+
+	void Update(float deltaTime) 
+	{
+		camera.Update(deltaTime);
+	}
+
+	void Render() 
+	{
+		DrawCubes();
+	}
+	
+protected:
+
+	void Init()
+	{
+		// init the vertex buffer object
+		InitVBO();
+
+		// init the indeces buffer object
+		InitIBO();
+
+		// init the vertex array object
+		InitVAO();
+
+		// load shader
+		shader.Load("assets/Shaders/basic.vert", "assets/Shaders/basic.frag");
+
+		// init cubes
+		InitCubes();
+
+		// init camera
+		camera.Init(glm::vec3(0.0f, 1.0f, -15.0f), glm::vec3(0.0f, 0.0f, 0.0f), 45.0f, 1024.0f / 768.0f, 0.1f, 1000000.0f);
+	}
+		
+	void InitVBO()
+	{
+		// create one buffer in the GPU, use it as an array buffer and set the data
+		glGenBuffers(1, &vertexBufferObject);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	}
+
+	void InitIBO()
+	{
+		// create one buffer in the GPU, use it as an element array buffer and set the data
+		glGenBuffers(1, &indexBufferObject);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	}
+
+	void InitVAO()
+	{
+		// create one vertex array object for drawing and use it. This vertexArrayObject is the one who will deal with the vertex buffer
+		glGenVertexArrays(1, &vertexArrayObject);
+		glBindVertexArray(vertexArrayObject);
+
+		// Tell the vertex shader how to interpret the buffer data. This information is needed for the active vertexArrayObject
+		// The 0 attribute(pos) has 3 elements (x,y,z) of type GL_FLOAT
+		// The stride to the next 0 attribute is zero bytes because there are no other attributes in between
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+		// Enable the 0 attribute
+		glEnableVertexAttribArray(0);
+	}
+
+	void InitCubes()
+	{
+		// floor
+		cubes[0].pos = glm::vec3(0.0f, 0.0f, 10.0f);
+		cubes[0].scale = glm::vec3(40.0f, 0.0001f, 40.f);
+		cubes[0].color = glm::vec4(0.8f, 0.8f, 0.8f, 1.0f);
+		cubes[0].enabled = true;
+	}
+
+	void DrawCubes()
+	{
+		const glm::mat4& viewProjection = camera.ViewProjectionMatrix();
+
+		// use the shader
+		shader.Use();	
+
+		// tell the vertexArrayObject to be used
+		glBindVertexArray(vertexArrayObject);
+
+		glm::mat4 model;
+		for (int i = 0; i < NUM_CUBES; i++)
+		{
+			Cube cube = cubes[i];
+			if (cube.enabled)
+			{
+				model = glm::mat4();
+				model = glm::translate(model, cube.pos) 
+					  * glm::rotate(model, cube.rotation.z, glm::vec3(0.0f, 0.0f, 1.0f)) 
+					  * glm::rotate(model, cube.rotation.y, glm::vec3(0.0f, 1.0f, 0.0f)) 
+					  * glm::rotate(model, cube.rotation.x, glm::vec3(1.0f, 0.0f, 0.0f))
+					  * glm::scale(model, cube.scale);
+				shader.SetUniform("modelViewProjection", viewProjection * model);
+				shader.SetUniform("color", cube.color);
+				glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, (void*)indices); // tell to draw cube by using the IBO
+			}
+		}
+
+		// do not use the vertexArrayObject anymore
+		glBindVertexArray(0);
+	}
+
+	void Terminate()
+	{
+		glDeleteVertexArrays(1, &vertexArrayObject);
+		glDeleteBuffers(1, &vertexBufferObject);
+		glDeleteBuffers(1, &indexBufferObject);
+	}
+
+	void ToggleWireframeMode()
+	{
+		wireframeMode = !wireframeMode;
+		glPolygonMode(GL_FRONT_AND_BACK, wireframeMode ? GL_LINE : GL_FILL);		
+	}
+
+private:
+
+	// vertices of the cube
+	GLfloat vertices[24] =
+	{
+		-1.0f,	1.0f,  1.0f,
+		 1.0f,  1.0f,  1.0f,
+		 1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
+
+		-1.0f,	1.0f, -1.0f,
+		 1.0f,  1.0f, -1.0f,
+		 1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f
+	};
+
+
+	// indices to cube vertices
+	GLuint indices[36] =
+	{
+	   // tri 1      tri 2
+		0, 2, 1,	0, 3, 2,	// front face
+		4, 6, 5,    4, 7, 6,	// back face	
+		4, 3, 0,    4, 7, 3,	// left face	
+		1, 6, 5,    1, 2, 6,	// right face	
+		4, 1, 5,    4, 0, 1,	// top face	
+		7, 2, 6,    7, 3, 2		// bottom face	
+	};
+	
+	// vbo, vao, ibo 
+	GLuint vertexBufferObject;
+	GLuint indexBufferObject;
+	GLuint vertexArrayObject; 
+	
+	// shader
+	Shader shader;
+
+	// cubes
+	struct Cube
+	{
+		bool enabled = false;
+
+		glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::vec3 rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::vec3 scale = glm::vec3(1.0f, 1.0f, 1.0f);
+		glm::vec4 color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	};
+
+	static const int NUM_CUBES = 64;
+	Cube cubes[NUM_CUBES];
+
+	// camera
+	FreeCamera camera;
+
+	bool wireframeMode = false;
+
+	
+};
+
+#endif
---
+++ TerrainGeneration/src/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+#define GLEW_STATIC
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
+
+#include "Input/Input.h"
+#include "TerrainGeneration/TerrainGeneration.h"
+
+#include <chrono>
+
+int main()
+{
+	// init glfw
+	if (!glfwInit())
+	{
+		std::cerr << "GLFW init failed!" << std::endl;
+		return -1;
+	}
+
+	// set window configuration
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // only support modern opengl
+
+	// create the window
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "SplineCam", nullptr, nullptr);
+	if (!window)
+	{
+		std::cerr << "GLFW failed to create the window!" << std::endl;
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	// Init glew
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "GLEW init failed!" << std::endl;
+		return -1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	// init SplineCam
+	TerrainGeneration splineCam;
+
+	// init input
+	Input::SetWindow(window);
+	Input::SetListener(static_cast<InputListener*>(&splineCam));
+	glfwSetKeyCallback(window, Input::OnKeyCallback);
+	glfwSetMouseButtonCallback(window, Input::OnMouseButtonCallback);
+	glfwSetScrollCallback(window, Input::OnMouseScrollCallback);
+	glfwSetCursorPosCallback(window, Input::OnMouseMoveCallback);
+
+	const auto frameCap = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::duration<long, std::ratio<1, 60>>(1));
+	auto deltaTime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::microseconds::zero());
+	auto previousTime = std::chrono::steady_clock::now();
+
+	// main loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		auto currentTime = std::chrono::steady_clock::now();
+		deltaTime += std::chrono::duration_cast<std::chrono::microseconds>(currentTime - previousTime);
+		previousTime = currentTime;
+
+		if (deltaTime > frameCap) 
+		{
+			glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			splineCam.Update((float)deltaTime.count() / 1000000);
+			splineCam.Render();
+
+			glfwSwapBuffers(window);
+
+			deltaTime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::microseconds::zero());
+		}
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
+
+
+
